ce58c11314cf699fc874b5e98fa8d78d2f834cce
N Manning <nmanning4@outlook.com>
2025-08-29 16:56:09 -0400

    Section 7

---
+++ web-server/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+app.get('', (req, res) =>{
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/help', (req, res) =>{
+    res.send({
+        name: 'Cleo', 
+        age: 36
+    })
+})
+
+app.get('/about', (req, res) =>{
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) =>{
+    res.send({
+        forecast: 'Snow',
+        location: 'Buffalo'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
